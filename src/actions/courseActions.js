0763d8d7699ea504eb194d4f3debc58943f1a8fb
Luke <luke.e.philips@gmail.com>
2017-10-12 15:33:22 -0700

    add some more thunks

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,5 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 // Action Creators
 export function loadCoursesSuccess(courses) {
@@ -16,6 +17,7 @@ export function updateCourseSuccess(course) {
 
 export function loadCourses() {
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
@@ -25,6 +27,7 @@ export function loadCourses() {
 }
 export function saveCourse(course) {
   return function(dispatch, getState) {
+    dispatch(beginAjaxCall());
     return courseApi.saveCourse(course).then(savedCourse => {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse))
